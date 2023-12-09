1a9f8d1f2690bf3a542d2b52812d1deb0596d1b7
Gabriele Matiddi <matiddi.1985899@studenti.uniroma1.it>
2023-12-09 12:18:36 +0100

    improved follow-user

--- service/api/follow-user.go
+++ service/api/follow-user.go
@@ -9,34 +9,38 @@ import (
 )
 
 func (rt *_router) followUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
-	// Extract parameters from the request path
+	// Extract parameters
 	userIDStr := ps.ByName("userId")
 	followIDStr := ps.ByName("followId")
+
+	//Extract userId
 	userID, err := strconv.Atoi(userIDStr)
 	if err != nil {
-		// Handle the error, for example, by returning a bad request response
 		w.Header().Set("content-type", "text/plain")
 		w.WriteHeader(http.StatusBadRequest)
 		_, _ = w.Write([]byte("Invalid userID"))
 		return
 	}
 
+	//Extract followId
 	followID, err := strconv.Atoi(followIDStr)
 	if err != nil {
-		// Handle the error, for example, by returning a bad request response
 		w.Header().Set("content-type", "text/plain")
 		w.WriteHeader(http.StatusBadRequest)
 		_, _ = w.Write([]byte("Invalid followID"))
 		return
 	}
+
+	//Database logic
 	err = rt.db.FollowUser(followID, userID)
 	if err != nil {
-		// Handle the error, for example, by returning an error response
 		w.Header().Set("content-type", "text/plain")
 		w.WriteHeader(http.StatusInternalServerError)
 		_, _ = w.Write([]byte("Error following user"))
 		return
 	}
+
+	//Write output
 	w.Header().Set("content-type", "text/plain")
 	_, _ = w.Write([]byte("Followed user successfully"))
 }
