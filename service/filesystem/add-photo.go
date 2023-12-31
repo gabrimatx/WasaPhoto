77fdf7e0e198daba6e3e18fdc3dcdaef8c930fa8
Gabriele Matiddi <matiddi.1985899@studenti.uniroma1.it>
2023-12-31 14:46:52 +0100

    Filesystem implementation added

--- service/filesystem/add-photo.go
+++ service/filesystem/add-photo.go
@@ -1,1 +1,29 @@
 package filesystem
+
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+)
+
+func saveBase64Photo(base64String, fileName string) error {
+	decoded, err := base64.StdEncoding.DecodeString(base64String)
+	if err != nil {
+		return fmt.Errorf("error decoding base64 string: %v", err)
+	}
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		return fmt.Errorf("error creating file: %v", err)
+	}
+	defer file.Close()
+
+	_, err = file.Write(decoded)
+	if err != nil {
+		return fmt.Errorf("error writing to file: %v", err)
+	}
+
+	fmt.Printf("Photo saved successfully as %s\n", fileName)
+
+	return nil
+}
--- service/filesystem/remove-photo.go
+++ service/filesystem/remove-photo.go
@@ -1,1 +1,18 @@
 package filesystem
+
+import (
+	"fmt"
+	"os"
+)
+
+func removePhoto(fileName string) error {
+	// Step 1: Remove the file
+	err := os.Remove(fileName)
+	if err != nil {
+		return fmt.Errorf("error removing file: %v", err)
+	}
+
+	fmt.Printf("Photo %s removed successfully\n", fileName)
+
+	return nil
+}
